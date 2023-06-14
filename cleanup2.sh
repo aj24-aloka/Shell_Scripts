637b84fa9f401edf2c2954a44cd8f2746284cb13
aj24-aloka <135518074+aj24-aloka@users.noreply.github.com>
2023-06-14 12:23:47 +0530

    Create cleanup2.sh
    using ls, awk, wc, head commands

---
+++ cleanup2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -x #to debug
+n=2 #delete old files except this number
+ls -lrt | awk '(NR>1){print $9}' >> list.txt
+count=$(wc -l < list.txt)
+num=$((  $count - $n ))
+he=$(head -n $num < list.txt)
+for file in $he; do
+        rm -f $file
+done
+echo "successfully deleted old files"
